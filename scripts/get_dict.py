28e03f30530853cad149406bc974f93ca4ddbc7e
George Close <closeg@cardiff.ac.uk>
2020-02-19 13:46:32 +0000

    added support to create dictionary and grammar from file

--- scripts/get_dict.py
+++ scripts/get_dict.py
@@ -1,15 +1,18 @@
 import requests
-url = "http://www.speech.cs.cmu.edu/cgi-bin/tools/logios/lextool.pl"
-#url = 'https://httpbin.org/post'
-files = {'wordfile': open('progs.txt','rb')}
-r = requests.post(url,files=files)
-for lines in r.text.split(">"):
-    if "<!-- DICT " in lines:
-        dl_link = lines
-print(dl_link)
-dl_link = dl_link.replace("<!-- DICT ","")
-dl_link = dl_link.replace("  --","")
-print(dl_link)
-r2 = requests.get(dl_link, allow_redirects=True)
-#print(r2.content)
-open('/home/g/year3/majel/languages/cmd2/progs1.dict', 'wb').write(r2.content)
+
+def get_dict(file_read,file_write):
+    url = "http://www.speech.cs.cmu.edu/cgi-bin/tools/logios/lextool.pl"
+    #url = 'https://httpbin.org/post'
+    files = {'wordfile': open(file_read,'rb')}
+    r = requests.post(url,files=files)
+    for lines in r.text.split(">"):
+        if "<!-- DICT " in lines:
+            dl_link = lines
+    print(dl_link)
+    dl_link = dl_link.replace("<!-- DICT ","")
+    dl_link = dl_link.replace("  --","")
+    print(dl_link)
+    r2 = requests.get(dl_link, allow_redirects=True)
+    open(file_write, 'wb').write(r2.content)
+
+get_dict("prog_list.txt","prog.dict")
--- scripts/get_programs.py
+++ scripts/get_programs.py
@@ -1,20 +1,33 @@
 from jsgf import PublicRule, RootGrammar
     
 def main():
+    upp_list = list()
     with open("progs.txt",'rt') as f:
         data = f.readlines()
-    grammar = RootGrammar(name="progs")
+    grammar = RootGrammar(name="progs",case_sensitive=True)
     i = 0
     for lines in data:
         rule_name = "rule" + str(i)
         #print(rule_name)
-        r = PublicRule(rule_name,lines)
-        grammar.add_rule(r)
-        i = i+1
+        upp= lines.upper().strip()
+        
 
-    print(grammar.compile())
+        if upp.isalpha() == True:
+            r = PublicRule(rule_name,upp,case_sensitive=True)
+            #print(r.generate())
+            #print(r)
+            grammar.add_rule(r)
+            upp_list.append(upp)
 
+            i = i+1
+    #print(grammar.compile())
 
+    with open("progs.gram",'wt') as g:
+        print(grammar.compile(),file=g)
+
+    with open("prog_list.txt",'wt') as f:
+        for e in upp_list:
+            print(e,file=f)
 
 if __name__ == '__main__':
     main()
